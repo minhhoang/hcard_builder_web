f38c630a363616ff5c94b3000e250668690b26eb
Quang Minh <minh_thanhtam@yahoo.com>
2015-04-22 00:13:09 +1000

    Trigger to auto show content when type in the textboxes

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="css/hcard.css">
 </head>
 
-<div class="site-wrapper">
+<div class="site-wrapper" ng-app="">
     <div class="main-content clearfix">
 		<div class="panel form">
 			<h1>hCard Builder</h1>
@@ -23,21 +23,21 @@
 				<div class="row">
 					<div class="col">
 						<label for="given_name">Given Name</label>
-						<input type="text" id="given_name" name="given_name">
+						<input type="text" id="given_name" name="given_name" ng-model="givenName">
 					</div>
 					<div class="col">
 						<label for="surname">Surname</label>
-						<input type="text" id="surname" name="surname">
+						<input type="text" id="surname" name="surname" ng-model="surName">
 					</div>
 				</div>
 				<div class="row">
 					<div class="col">
 						<label for="email">Email</label>
-						<input type="text" id="email" name="email">
+						<input type="text" id="email" name="email" ng-model="email">
 					</div>
 					<div class="col">
 						<label for="phone">Phone</label>
-						<input type="text" id="phone" name="phone">
+						<input type="text" id="phone" name="phone" ng-model="phone">
 					</div>
 				</div>
 				<div class="info-title">
@@ -46,31 +46,31 @@
 				<div class="row">
 					<div class="col">
 						<label for="house_name">House Name or #</label>
-						<input type="text" id="house_name" name="house_name">
+						<input type="text" id="house_name" name="house_name" ng-model="houseName">
 					</div>
 					<div class="col">
 						<label for="street">Street</label>
-						<input type="text" id="street" name="street">
+						<input type="text" id="street" name="street" ng-model="street">
 					</div>
 				</div>
 				<div class="row">
 					<div class="col">
 						<label for="suburb">Suburb</label>
-						<input type="text" id="suburb" name="suburb">
+						<input type="text" id="suburb" name="suburb" ng-model="suburb">
 					</div>
 					<div class="col">
 						<label for="state">State</label>
-						<input type="text" id="state" name="state">
+						<input type="text" id="state" name="state" ng-model="state">
 					</div>
 				</div>
 				<div class="row">
 					<div class="col">
 						<label for="postcode">Postcode</label>
-						<input type="text" id="postcode" name="postcode">
+						<input type="text" id="postcode" name="postcode" ng-model="postcode">
 					</div>
 					<div class="col">
 						<label for="country">Country</label>
-						<input type="text" id="country" name="country">
+						<input type="text" id="country" name="country" ng-model="country">
 					</div>
 				</div>
 				<div class="row"><button>Create hCard</button></div>
@@ -81,29 +81,30 @@
 				<div class="hcard-preview-title">
 					hcard Preview
 				</div>
-				<h2>John Fairfax</h2>
+				<h2>{{ givenName }} {{ surName }} &nbsp;</h2>
 				<div class="content-preview clearfix">
 					<div class="row">
-						<div class="title">Email</div><div class="value email">john.fairfax@fairfaxmedia.com.au</div>
+						<div class="title">Email</div><div class="value email">{{ email }}&nbsp;</div>
 					</div>
 					<div class="row">
-						<div class="title">Phone</div><div class="value phone">02 9282 2833</div>
+						<div class="title">Phone</div><div class="value phone">{{ phone }}&nbsp;</div>
 					</div>
 					<div class="row">
-						<div class="title">Address</div><div class="value address">1 Darling Island Road</div>
+						<div class="title">Address</div><div class="value address">{{ houseName }} {{ street }}&nbsp;</div>
 					</div>
 					<div class="row">
-						<div class="title hide-on-phone">&nbsp;</div><div class="value suburb">Pyrmont, NSW</div>
+						<div class="title hide-on-phone">&nbsp;</div><div class="value suburb">{{ suburb }}<span class="comma">,</span> {{ state }}&nbsp;</div>
 					</div>
 					<div class="row">
-						<div class="title">Postcode</div><div class="value postcode">2009</div>
-						<div class="title country-name">Country</div><div class="value country">Australia</div>
+						<div class="title">Postcode</div><div class="value postcode">{{ postcode }}&nbsp;</div>
+						<div class="title country-name">Country</div><div class="value country">{{ country }}&nbsp;</div>
 					</div>
 				</div>
 			</div>
 		</div>
     </div>
 </div>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
 <script type="text/javascript" src="js/compiled.js"></script>
 </body>
 </html>
--- public/js/hcard.js
+++ public/js/hcard.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    
+    $('#suburb').change(function(){
+    	console.log("abc");
+    	var current = $(this);
+    	if (current.val() != '') {
+    		$('.comma').css('display', 'inline');
+    	} else {
+    		$('.comma').css('display', 'none');
+    	}
+    });
 });
